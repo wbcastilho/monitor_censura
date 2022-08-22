644400af084e47f93419c12aa1956fa9c17d7e6f
wbcastilho <welber.castilho@yahoo.com.br>
2022-08-21 21:32:55 -0300

    Adicionado o metodo check_process_exist na classe MyPsutil

--- MainForm.py
+++ MainForm.py
@@ -1,11 +1,10 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from tkinter import filedialog
-import os
 from tkinter import messagebox
+from ClientMqtt import ClientMqtt
 from MyPsutil import MyPsutil
-import json
 from Config import Config
+import json
 
 
 class MainForm(ttk.Frame):
@@ -108,6 +107,22 @@ class MainForm(ttk.Frame):
         self.button_save = ttk.Button(frame, text="Salvar Configuração", bootstyle="default", command=self.on_save)
         self.button_save.pack(side=RIGHT, pady=10)
 
+    def on_connect(self):
+        print("Conectar")
+        # if not self.validate_form():
+        try:
+            self.client_mqtt = ClientMqtt("MONITOR_PC", self.topic.get(), self.server.get(), int(self.port.get()))
+            self.change_buttons_state(True)
+            self.after(0, self.loop)
+        except Exception as err:
+            messagebox.showerror(title="Erro", message=err)
+
+    def on_disconnect(self):
+        print("Desconectar")
+        self.client_mqtt.loop_stop()
+        self.change_buttons_state(False)
+        self.after_cancel(self.afterid.get())
+
     def on_save(self):
         # if not self.validate_form():
         config = Config(self.server.get(), self.port.get(), self.topic.get(), self.process.get(),
@@ -120,6 +135,52 @@ class MainForm(ttk.Frame):
         except Exception:
             messagebox.showerror(title="Erro", message="Falha ao salvar arquivo de configuração.")
 
+    def loop(self):
+        def show_message_error(message):
+            self.change_buttons_state(False)
+            self.after_cancel(self.afterid.get())
+            messagebox.showerror(title="Erro", message=message)
+
+        try:
+            if self.client_mqtt.connected:
+                result = MyPsutil.check_process_exist(self.process.get())
+
+                if result:
+                    print("Gera alarme")
+                    self.client_mqtt.publish(self.topic_process.get(), '1')
+                else:
+                    print("Sem alarme")
+                    self.client_mqtt.publish(self.topic_process.get(), '0')
+
+                self.afterid.set(self.after(5000, self.loop))
+            else:
+                show_message_error("Cliente desconectado do broker de maneira inesperada.")
+
+        except FileNotFoundError as err:
+            show_message_error(err)
+        except Exception as err:
+            show_message_error(err)
+
+    def change_buttons_state(self, state):
+        if state:
+            self.button_connect["state"] = "disabled"
+            self.button_disconnect["state"] = "normal"
+            self.button_browser["state"] = "disabled"
+            self.button_save["state"] = "disabled"
+            self.entry_server["state"] = "readonly"
+            self.entry_port["state"] = "readonly"
+            self.entry_topic["state"] = "readonly"
+            self.combobox_process["state"] = "readonly"
+        else:
+            self.button_connect["state"] = "normal"
+            self.button_disconnect["state"] = "disabled"
+            self.button_browser["state"] = "normal"
+            self.button_save["state"] = "normal"
+            self.entry_server["state"] = "normal"
+            self.entry_port["state"] = "normal"
+            self.entry_topic["state"] = "normal"
+            self.combobox_process["state"] = "normal"
+
     def read_config(self):
         try:
             with open('config.json', 'r') as f:
@@ -130,9 +191,9 @@ class MainForm(ttk.Frame):
                 self.topic.set(config.topic)
                 self.process.set(config.process)
                 self.topic_process.set(config.topic_process)
-        except PermissionError as err:
+        except PermissionError:
             messagebox.showwarning(title="Atenção", message="Sem permissão para abrir o arquivo de configuração.")
-        except FileNotFoundError as err:
+        except FileNotFoundError:
             messagebox.showwarning(title="Atenção", message="Arquivo de configuração não encontrado.")
         except Exception:
             messagebox.showwarning(title="Atenção", message="Falha ao abir arquivo de configuração.")
